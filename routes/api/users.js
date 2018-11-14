468137d8361186b625d24b1f8b43ced054e54658
Bidyut Deka <bideka03@in.ibm.com>
2018-11-14 17:33:33 +0530

    create The JWT

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const User = require("../../models/User");
 const gravater = require("gravatar");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const keys = require("../../config/keys");
 
 // @route   GET /api/users/test
 // @desc    Tests users route
@@ -53,6 +55,7 @@ router.post("/login", (req, res) => {
 
   // Find User By Email
   User.findOne({ email }).then(user => {
+    // Check for User
     if (!user) {
       return res.status(404).json({ email: "User not found" });
     }
@@ -60,7 +63,21 @@ router.post("/login", (req, res) => {
     // Check Password
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
-        res.json({ msg: "Success" });
+        //User Matched
+        const payload = { id: user.id, name: user.name, avatar: user.avatar }; // Create JWT Payload
+
+        // Sign Token
+        jwt.sign(
+          payload,
+          keys.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: "Bearer " + token
+            });
+          }
+        );
       } else {
         return res.status(400).json({ password: "Password Incorrect" });
       }
